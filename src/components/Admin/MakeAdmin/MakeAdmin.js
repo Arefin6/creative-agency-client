882383d9375c818947a13e2f3f2df2c4664b6d35
Arefi6 <arefinhossain3@gmail.com>
2020-10-17 12:56:22 +0600

    insert admin db done

--- src/components/Admin/MakeAdmin/MakeAdmin.js
+++ src/components/Admin/MakeAdmin/MakeAdmin.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
 import DashboardHeader from '../../Dashboard/DashboardHeader/DashboardHeader';
 import Sidebar from '../../Dashboard/Sidebar/Sidebar';
 
 const MakeAdmin = () => {
+    const { handleSubmit, register, errors } = useForm();
+    const onSubmit = (values,e) =>{
+          
+        fetch('http://localhost:5000/addAdmin', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(values)
+        })
+        .then(res => res.json())
+        .then(data => {
+           if(data){
+             alert("congratulations!! You are admin now");
+           }
+        })
+              
+
+        e.preventDefault();
+    }   
+
+
+
+   
+
     return (
         <div>
             <div>
@@ -13,10 +39,13 @@ const MakeAdmin = () => {
                  <Sidebar></Sidebar>
                 </div> 
                 <div className="col-md-8 makeAdmin-container">
-                    <form>
+                    <form onSubmit={handleSubmit(onSubmit)}>
                     <label htmlFor="Email">Email</label>
                         <div className="d-flex">
-                        <input type="email"  className="form-control w-50 mr-3" placeholder="email"/>
+                        <input type="email" name="email"
+                         ref={register({ required: true })}
+                        className="form-control w-50 mr-3" placeholder="email"/>
+                          {errors.email && <span className="text-danger">This field is required</span>}  
                           <input type="submit" className=" btn btn-success"/>
                         </div>
                     </form>
